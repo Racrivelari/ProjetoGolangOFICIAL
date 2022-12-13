fed7ca3f7f88f77d8215be103b26025951e343ac
Racrivelari <ramon.crivelari@gmail.com>
2022-12-13 08:28:14 -0300

    front end + request postman funcionando

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
+
+	// "log"
 	"net/http"
 	"os"
 
@@ -28,8 +29,8 @@ func main() {
 	dbpool := database.NewDB(conf)
 	service := service.NewProdutoService(dbpool)
 
-	println(conf.DB_DRIVE)
-	println(conf.DB_DSN)
+	println("Driver utilizado: ", conf.DB_DRIVE)
+	println(("Banco de dados: "), conf.DB_NAME)
 
 	r := mux.NewRouter()
 	n := negroni.New(
@@ -39,10 +40,19 @@ func main() {
 	r.HandleFunc("/", redirect)
 	handler.RegisterAPIHandlers(r, n, service)
 	fmt.Println("Escutando na porta 5000")
-	log.Fatal(http.ListenAndServe(":5000", r))
+	//log.Fatal(http.ListenAndServe(":5000", r))
+
+	fs := http.FileServer(http.Dir("webui/dist/spa"))
+	r.Handle("/webui/", http.StripPrefix("/webui/", fs)) //aq vc passa o r inves do http.handle, pq vc criou suas proprias rotas no router, ou seja vc n ta usando padrao go e sim um proprio
+	http.ListenAndServe(":5000", r)                      //tem q ser r, eu só passaria nil aq, se eu n tivesse feito minhas rotas proprias
+
+	//DEU CERTO COM ESSE JEITO DO ISAEL AQ, funcionou o frontend, e tipo, eu tento logar ele da uma msg verde de "bem vindo admin" porem n redireciona pra home n sei pq
+	//outro ponto, eu tirei a main da pasta raiz e joguei pra cmd dnv, ele voltou a funcionar nao faco ideia pq KKKKKKKKKKKKKKK
+
+	//VC PODE PULAR A TELA DE LOGIN, VC COLOCA O ENDERECO/PRODUCTS, dai pula
+
 }
 
 func redirect(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/webui/", http.StatusMovedPermanently)
 }
-
